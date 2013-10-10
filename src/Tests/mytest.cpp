2800bb677d96845a248206664576a73158725611
michael lloyd weschler <mwesc001@jccl-33.ad.cs.fiu.edu>
2013-10-10 17:41:08 -0400

    Tests build now

--- src/Tests/mytest.cpp
+++ src/Tests/mytest.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
 #include "gtest/gtest.h"
 #include "core.h"
 
 TEST(coretest, validtest){
 	int num = dostuff();
 	EXPECT_EQ(num, 15);
+}
+
+int main(int argc, char **argv){
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
