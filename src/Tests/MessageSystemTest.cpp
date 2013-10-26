5bb201ce1ee1fc5b9666c034b3b732841bceb829
robert o law <rlaw002@jccl-18.ad.cs.fiu.edu>
2013-10-26 02:03:49 -0400

    fix message system fail

--- src/Tests/MessageSystemTest.cpp
+++ src/Tests/MessageSystemTest.cpp
@@ -6,13 +6,20 @@
 template <typename T>
 class Observer1:public Observer<T>
 {
-		std::string theMessage;
+public: std::string theMessage;
 		virtual void update(T *message)
 		{
-
+		//	std::cout << "yolo Swag /n";
 			 theMessage = *message;
 
 		}
+		 std::string who; 
+	//	 void hey()
+	//	 {
+			
+//			std::cout << "MIKE IS SWINDLER      sss  ";
+//		 }
+		
 	};
 
 template <typename T>
@@ -25,6 +32,7 @@ class Observer2:public Observer<T>
 			 theMessage = *message;
 
 		}
+	//	std::string who ="Im Observer 2 /n";
 	};
 
 
@@ -39,21 +47,31 @@ class Observer3:public Observer<T>
 			 theMessage = *message;
 
 		}
+//		std::string who ="Im Observer 3 /n";
 	};
 
 TEST(messageTest, test_one)
 {
 	std::string theMessage;
+	theMessage ="Yolo Swag";
 	Observer1<std::string> observer1;
 	Observer2<std::string> observer2;
 	Observer3<std::string> observer3;
 
-
+	observer1.who = "WE IS TESTING STUFF/n";
 	Subject<std::string> subject;
-	subject.attach(&(Observer<std::string>)observer1);
-	subject.attach(&(Observer<std::string>)observer2);
-	subject.attach(&(Observer<std::string>)observer3);
+	subject.attach(((Observer<std::string>)observer1));
 	
+	//subject.attach(&(Observer<std::string>)observer2);
+//	subject.attach(&(Observer<std::string>)observer3);
+	//std::cout <<(((Observer1<std::string>*)subject.list.at(0)) ->who);
+
+//	std::cout << subject.list.at(1);
+	//std::cout << subject.list.at(2);
+	//subject.notifyObservers(theMessage);
+
+
+//	EXPECT_EQ("yolo Swag", observer1.theMessage);
 
 
 
--- src/Tests/mytest.cpp
+++ src/Tests/mytest.cpp
@@ -3,20 +3,8 @@
 #include "Logger.h"
 #include <stdio.h>  
 
-<<<<<<< HEAD
-
-TEST(coretest, validtest){
-	EXPECT_EQ(15, 15);
-}
-
-
-
-int main(int argc, char **argv){
-=======
-
 int main(int argc, char **argv)
 {
->>>>>>> 07274bbcacb2f33c1b497b2868dbcb7bc18b2870
 	::testing::InitGoogleTest(&argc, argv);
 	int rv = RUN_ALL_TESTS();
 	std::getchar();
