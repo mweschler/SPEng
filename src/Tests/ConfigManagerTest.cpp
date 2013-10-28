379a1a30ae6d96f4267a05d3c47674dce3c0b2b7
Brian <lara.brian@hotmail.com>
2013-10-27 23:32:45 -0400

    changed config manager test suite

--- src/Tests/ConfigManagerTest.cpp
+++ src/Tests/ConfigManagerTest.cpp
@@ -3,29 +3,27 @@
 #include "ConfigurationManager.h"
 #include "VariableContainer.h"
 
-TEST(ConfigSuite, storingAndRetrievingIntVariableTest){
 	ConfigurationManager configurationManager;
+
+TEST(ConfigSuite, storingAndRetrievingIntVariableTest){
 	configurationManager.setVariable("Variable1",4);
 	int result = configurationManager.getVariable<int>("Variable1");
 	EXPECT_EQ(result,4);
 }
 
 TEST(ConfigSuite, storingAndRetrievingDoubleVariableTest){
-	ConfigurationManager configurationManager;
 	configurationManager.setVariable("Variable2",4.0);
 	double result = configurationManager.getVariable<double>("Variable2");
 	EXPECT_EQ(result,4.0);
 }
 
 TEST(ConfigSuite, storingAndRetrievingFloatVariableTest){
-	ConfigurationManager configurationManager;
 	configurationManager.setVariable("Variable3",4.0f);
 	float result = configurationManager.getVariable<float>("Variable3");
 	EXPECT_EQ(result,4.0f);
 }
 
 TEST(ConfigSuite, storingAndRetrievingStringVariableTest){
-	ConfigurationManager configurationManager;
 	std::string temp = "4.0";
 	configurationManager.setVariable("Variable4",temp);
 	std::string result = configurationManager.getVariable<std::string>("Variable4");
@@ -33,7 +31,6 @@ TEST(ConfigSuite, storingAndRetrievingStringVariableTest){
 }
 
 TEST(ConfigSuite, storingAndRetrievingCharVariableTest){
-	ConfigurationManager configurationManager;
 	configurationManager.setVariable("Variable5",'4');
 	char result = configurationManager.getVariable<char>("Variable5");
 	EXPECT_EQ(result,'4');
