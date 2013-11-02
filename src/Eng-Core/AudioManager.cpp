0af6f284f87fd88dc727d93d1bffb2c1869ecf10
brian lara <blara002@jccl-21.ad.cs.fiu.edu>
2013-11-02 00:30:30 -0400

    Integrated port audio api and created audio manager c++ files

---
+++ src/Eng-Core/AudioManager.cpp
@@ -0,0 +1,3 @@
+#include <String>
+#include "AudioManager.h"
+
---
+++ src/Tests/AudioManagerTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "AudioManager.h"
+#include "portaudio.h"
+
+TEST(AudioSuite, playingAudioFile){
+	int result = 4;
+	
+	EXPECT_EQ(4,result);
+}
+
