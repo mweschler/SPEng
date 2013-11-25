f81a4ef5abb74cd7799114e7f298217709d55563
Julian Nodarse <jnoda009@fiu.edu>
2013-11-25 14:36:53 -0500

    improved logger

--- src/Eng-Core/AssetManager.cpp
+++ src/Eng-Core/AssetManager.cpp
@@ -51,15 +51,7 @@ AssetManager* AssetManager::instance = NULL;
 			}
 			
 		}
-		
-
-	    
-		//string currentAssetName;
-		
-		/*
-		string currentAssetLocation;
-		map<string, Asset> assetStorage;
-		*/
+	
 		bool AssetManager::checkForDuplicate(string name)
 		{
 			try
@@ -72,20 +64,13 @@ AssetManager* AssetManager::instance = NULL;
 				return false;
 			}
 		}
-		/*
-		int createAsset(Asset asset);
-		int addAsset(string name);
-	    int increaseRefcount(string name);
-		int decreaseRefCount(string name);
-		*/
+		
+		
 
 		int AssetManager::getRefCount(string name)
 		{
 			return assetStorage.at(name) -> getRefCount();		
 		}
-		/*
-		static AssetManager* instance;
-		AssetManager();
-		AssetManager(AssetManager const&);
-		AssetManager& operator=(AssetManager const&);
-		*/
+		
+		
+		
